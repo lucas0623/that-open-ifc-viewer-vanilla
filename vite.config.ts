5b87d955e83a894cb43b3ca5edd31d4524356425
lucas0623 <51280021+lucas0623@users.noreply.github.com>
2025-06-15 20:50:24 +0800

    update vite config

---
+++ vite.config.ts
@@ -0,0 +1,5 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/that-open-ifc-viewer-vanilla/', // Replace with your actual repository name, e.g., '/ifc-viewer/'
+});
